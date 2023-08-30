a7ade92650f7d546d78cc3d2869f7c688a398f49
goodpeanuts <goodpeanuts@foxmail.com>
2023-08-30 23:40:15 +0800

    no flush in index

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,20 +1,20 @@
 <template>
     <nav class="navbar navbar-expand-lg  bg-body-tertiary">
   <div class="container">
-    <a class="navbar-brand" href="#">King Of Bots</a>
+    <router-link  class="navbar-brand" :to="{name: 'home'}">King of Bots</router-link> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">对战</a>
+          <router-link class="nav-link" :to="{name: 'pk_index'}">对战</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">对战列表</a>
+          <router-link class="nav-link" :to="{name: 'record_index'}">对局列表</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">排行榜</a>
+          <router-link class="nav-link" :to="{name: 'ranklist_index'}">排行榜</router-link>
         </li>
       </ul>
       <ul class="navbar-nav  ">
@@ -23,7 +23,9 @@
             用户
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">我的BOT</a></li>
+            <li>
+              <router-link class="dropdown-item" :to="{name: 'user_bot_index'}" href="/user/bot/">我的bot</router-link>
+            </li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">退出</a></li>
           </ul>
