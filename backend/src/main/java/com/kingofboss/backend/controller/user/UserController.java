856956477266ade80d73f43d5a74f088ab7e44df
goodpeanuts <goodpeanuts@foxmail.com>
2023-09-02 15:11:52 +0800

    realize search user by id

--- backend/src/main/java/com/kingofboss/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kingofboss/backend/controller/user/UserController.java
@@ -4,6 +4,8 @@ package com.kingofboss.backend.controller.user;
 import com.kingofboss.backend.mapper.UserMapper;
 import com.kingofboss.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +20,10 @@ public class UserController {
     public List<User> getAll() {
     return userMapper.selectList(null);
     }
+
+    @GetMapping("/user/{userId}/")
+    public User getuer(@PathVariable int userId) {
+        return userMapper.selectById(userId);
+    }
+
 }
